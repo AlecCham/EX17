6ec3fa85f6d9146ec8dd35a622d35cbe4b6a3f78
Alec Chaprak <achaprak@gmail.com>
2024-01-02 19:02:00 -0800

    Updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,14 @@ const Navbar = () => {
     <div>
     <nav className="navbar navbar-expand-lg navbar-dark fixed-top" style={{ backgroundColor: '#f8c291' }}>
       <div className="container">
-        <a className="navbar-brand" href="/" style={{ color: 'chocolate' }}>Little Village</a>
+        <a className="navbar-brand" href="/home" style={{ color: 'chocolate' }}>Little Village</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <a className="nav-link" href="/" style={{ color: 'chocolate' }}>Home</a>
+              <a className="nav-link" href="/home" style={{ color: 'chocolate' }}>Home</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="/aboutus" style={{ color: 'chocolate' }}>About Us</a>
