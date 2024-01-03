d189796dc625878e83a340bbb46b42c629efb5c4
Alec Chaprak <achaprak@gmail.com>
2024-01-02 17:45:27 -0800

    Updated path for pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,19 +12,19 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <a className="nav-link" href="#home" style={{ color: 'chocolate' }}>Home</a>
+              <a className="nav-link" href="/home" style={{ color: 'chocolate' }}>Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#about-us" style={{ color: 'chocolate' }}>About Us</a>
+              <a className="nav-link" href="/about-us" style={{ color: 'chocolate' }}>About Us</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#gallery" style={{ color: 'chocolate' }}>Gastronomic Gems</a>
+              <a className="nav-link" href="/gallery" style={{ color: 'chocolate' }}>Gastronomic Gems</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#contact" style={{ color: 'chocolate' }}>Contact</a>
+              <a className="nav-link" href="/contact" style={{ color: 'chocolate' }}>Contact</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#login" style={{ color: 'chocolate' }}>Login</a>
+              <a className="nav-link" href="/login" style={{ color: 'chocolate' }}>Login</a>
             </li>
           </ul>
         </div>
